9158d478e3f131bd6e2597bbed2e03436b817b5f
Arturo <templariojc@gmail.com>
2021-06-08 17:09:19 -0700

    first commit

---
+++ src/test/java/qaacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test 
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+	
+	
+	
+}
---
+++ src/test/java/qaacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void BrowserAutomation() 
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+
+}
