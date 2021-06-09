951ec3056a815ffe8c4d11301170749c6089a171
Arturo <templariojc@gmail.com>
2021-06-09 13:34:58 -0700

    final commit after merge develop

---
+++ src/test/java/qaacademy/Mavenjava/newClass.java
@@ -0,0 +1,10 @@
+package qaacademy.Mavenjava;
+
+public class newClass {
+	public void NativeAppAndroid2()
+	{
+		System.out.println("NativeAppAndroid");
+		System.out.println("NativeAppAndroid");
+	}
+
+}
